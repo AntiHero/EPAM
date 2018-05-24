93751fc9dc88a3bce364b8f7ca480ce3a834e35c
Aleksandr Schemelev <prophreaker@gmail.com>
2018-05-24 22:14:31 +0300

    Add files via upload

---
+++ TASK1/src/Creature.java
@@ -0,0 +1,31 @@
+
+class Creature {
+    public String name;
+    public int weight;
+    public int age;
+}
+
+class Animal extends Creature {
+
+    public String name;
+    public int age;
+
+    /* Constructor */
+    public Animal(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String Innoculate(int age) {
+
+        String status;
+
+        if (age < 3) {
+            status = "Underaged for innoculation...";
+
+        } else {
+            status = "Successfully innoculated!";
+        }
+        return status;
+    }
+}
