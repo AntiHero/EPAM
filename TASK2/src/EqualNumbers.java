99984b19392a7b6bf32769951e91b7cac577b012
Aleksandr Schemelev <prophreaker@gmail.com>
2018-05-26 12:22:44 +0300

    Update EqualNumbers.java

--- TASK2/src/EqualNumbers.java
+++ TASK2/src/EqualNumbers.java
@@ -2,6 +2,7 @@
 public class EqualNumbers {
     public static String compareTwoNumbers(int num1, int num2) {
         String str;
+        
         str = (num1 == num2) ? "equal" : "not equal";
         
         return str;
