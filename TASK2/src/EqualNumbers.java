b9e625ada29d42108ef6fa02b5133113db8aa4a4
Aleksandr Schemelev <prophreaker@gmail.com>
2018-05-26 12:17:39 +0300

    Delete EqualNumbers.java

--- TASK2/src/EqualNumbers.java
+++
@@ -1,9 +0,0 @@
-
-public class EqualNumbers {
-    public static String compareTwoNumbers(int num1, int num2) {
-        String str;
-        str = (num1 == num2) ? "equal" : "not equal";
-        
-        return str;
-    }
-}
