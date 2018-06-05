75c96a1e86ea39bbf1be6701e3a2b49061298c20
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-06 00:44:45 +0300

    Update Algorithms.java

--- TASK4/src/Algorithms.java
+++ TASK4/src/Algorithms.java
@@ -1,119 +1,104 @@
 
 public class Algorithms {
-    public static int findMaxNatural(int number) {
-        int max_value = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+    private static final int TEN = 10;
 
-        boolean check = check(number);
+    public static int findMaxNatural(int number) throws Exception {
 
-        if (check) {
-            int length = String.valueOf(number).length();
+        InvalidException.isInvalidException(number);
 
-            for (int i = 0; i < length; i++) {
-                if (number % 10 >= max_value) {
-                    max_value = number % 10;
-                    number /= 10;
-                }
+        int max_value = number % TEN;
+        int last_digit = max_value;
+
+        while ((number /= TEN) > 0) {
+            last_digit = number % TEN;
+
+            if (last_digit > max_value) {
+                max_value = last_digit;
             }
         }
+
         return max_value;
     }
 
-    
-    public static String findPalindrome(int number) {
-        String result = "It's not a palindrome!";
+    public static String findPalindrome(int number) throws Exception {
+        
+        InvalidException.isInvalidException(number);
 
         int reverse = 0;
-        int remainder = 0;
         int palindrome = number;
 
-        boolean check = check(number);
-
-        if (check) {
-            while (palindrome != 0) {
-                remainder = palindrome % 10;
-                reverse = 10 * reverse + remainder;
-                palindrome /= 10;
-            }
-            if (number == reverse) {
-                result = "It's a palindrome!";
-            }
+        while (palindrome > 0) {
+            reverse = TEN * reverse + palindrome % TEN;
+            palindrome /= TEN;
         }
-        return result;
+
+        return (number == reverse) ? "It's a palindrome!" : "It's not a palindrome!";
     }
 
-    
-    public static String isSimpleNatural(int number) {
-        String result = "Invalid number!";
+    public static boolean isSimpleNatural(int number) throws Exception {
 
-        boolean check = check(number);
+        InvalidException.isInvalidException(number);
 
-        if (check) {
-            for (int i = 2; i <= number; i++) {
-                if (i != number) {
-                    if (number % i == 0) {
-                        result = "Not a simple natural!";
-                        break;
-                    }
-                } 
-                else {
-                    result = "Simple natural!";
-                }
+        boolean result = true;
+
+        for (int i = TWO; i <= number / TWO; i++) {
+            if (number % i == 0) {
+                result = false;
+                break;
             }
         }
-        return result;
+        return (number == 1) ? false : result;
     }
 
-    
-    public static String findSimpNatDivider(int number) {
-        String result = "Invalid number!";
+    public static String findSimpNatDivider(int number) throws Exception {
 
-        boolean check = checkZero(number);
+        InvalidException.isInvalidException(number);
 
-        if (check) {
-            result = "";
-            for (int i = 2; i <= number; i++) {
+        String result = "";
+
+        if (number != 1 && !isSimpleNatural(number)) {
+            for (int i = TWO; i <= number / TWO; i++) {
                 if (number % i == 0) {
-                    for (int j = 2; j <= i; j++) {
-                        if (i != j) {
-                            if (i % j == 0) {
-                                break;
-                            }
-                        } 
-                        else {
-                            result += j + " ";
-                        }
+                    if (isSimpleNatural(i)) {
+                        result += i + " ";
                     }
                 }
             }
+        } 
+        else {
+            result = (number > 1) ? Integer.toString(number) : "No simple dividers";
         }
         return result;
     }
 
-    
-    public static String findGCD(int number_1, int number_2) {
-        String result = "LCM and GCD not found! ";
-        int operand_1 = number_1;
-        int operand_2 = number_2;
+    public static int findGCD(int number_1, int number_2) throws Exception {
 
-        if (checkZero(operand_1) && checkZero(operand_2)) {
-            if (operand_1 != operand_2) {
-                while (operand_1 != operand_2) {
-                    if (operand_1 > operand_2) {
-                        operand_1 -= operand_2;
-                    } 
-                    else {
-                        operand_2 -= operand_1;
-                    }
+        InvalidException.isInvalidException(number_1);
+        InvalidException.isInvalidException(number_2);
+
+        if (number_1 != number_2) {
+            while (number_1 != number_2) {
+                if (number_1 > number_2) {
+                    number_1 -= number_2;
+                } else {
+                    number_2 -= number_1;
                 }
             }
-            result = "GCD is: " + operand_1 + ", LCD is: " + number_1 * number_2 / operand_1;
         }
-        return result;
+        return number_1;
     }
 
-    
-    public static int findDifDigits(int number) {
-        int result = 0;
+    public static int findLCM(int number_1, int number_2) throws Exception {
+
+        InvalidException.isInvalidException(number_1);
+        InvalidException.isInvalidException(number_1);
+
+        return number_1 * number_2 / findGCD(number_1, number_2);
+    }
+
+    public static int findDifDigits(int number) throws Exception {
         int remainder = 0;
 
         int num_0 = 0;
@@ -127,104 +112,45 @@ public class Algorithms {
         int num_8 = 0;
         int num_9 = 0;
 
-        boolean check = check(number);
+        InvalidException.isInvalidException(number);
 
-        if (check) {
-            int length = String.valueOf(number).length();
-
-            while (length > 0) {
-                remainder = number % 10;
-                
-                switch (remainder) {
-                case 0:
-                    num_0++;
-                    if (num_0 > 1) {
-                        num_0 = 1;
-                    }
-                    break;
-                case 1:
-                    num_1++;
-                    if (num_1 > 1) {
-                        num_1 = 1;
-                    }
-                    break;
-                case 2:
-                    num_2++;
-                    if (num_2 > 1) {
-                        num_2 = 1;
-                    }
-                    break;
-                case 3:
-                    ++num_3;
-                    if (num_3 > 1) {
-                        num_3 = 1;
-                    }
-                    break;
-                case 4:
-                    num_4++;
-                    if (num_4 > 1) {
-                        num_4 = 1;
-                    }
-                    break;
-                case 5:
-                    num_5++;
-                    if (num_5 > 1) {
-                        num_5 = 1;
-                    }
-                    break;
-                case 6:
-                    num_6++;
-                    if (num_6 > 1) {
-                        num_6 = 1;
-                    }
-                    break;
-                case 7:
-                    num_7++;
-                    if (num_7 > 1) {
-                        num_7 = 1;
-                    }
-                    break;
-                case 8:
-                    num_8++;
-                    if (num_8 > 1) {
-                        num_8 = 1;
-                    }
-                    break;
-                case 9:
-                    num_9++;
-                    if (num_9 > 1) {
-                        num_9 = 1;
-                    }
-                    break;
-                }
-                
-                number /= 10;
-                length--;
+        while (number > 0) {
+            remainder = number % TEN;
+            switch (remainder) {
+            case 1:
+                num_1 = ONE;
+                break;
+            case 2:
+                num_2 = ONE;
+                break;
+            case 3:
+                num_3 = ONE;
+                break;
+            case 4:
+                num_4 = ONE;
+                break;
+            case 5:
+                num_5 = ONE;
+                break;
+            case 6:
+                num_6 = ONE;
+                break;
+            case 7:
+                num_7 = ONE;
+                break;
+            case 8:
+                num_8 = ONE;
+                break;
+            case 9:
+                num_9 = ONE;
+                break;
+            default:
+                num_0 = ONE;
+                break;
             }
-            
-            result = num_0 + num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9;
+            number /= TEN;
         }
-        
-        return result;
+        return num_0 + num_1 + num_2 + num_3 + num_4 + num_5 + num_6 + num_7 + num_8 + num_9;
     }
-    
 
-    public static boolean check(int number) {
-        boolean result = true;
-
-        if (number < 0) {
-            result = false;
-        }
-        return result;
-    }
-    
-
-    public static boolean checkZero(int number) {
-        boolean result = true;
-
-        if (number <= 0) {
-            result = false;
-        }
-        return result;
-    }
 }
