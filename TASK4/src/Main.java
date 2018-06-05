0f8278647dd2a30d6b222aa463402a526a206a2f
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-06 00:49:22 +0300

    Update Main.java

--- TASK4/src/Main.java
+++ TASK4/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     /**
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO Auto-generated method stub
         ProgramTester.headstailsTester();
         ProgramTester.algorithmsTest();
