cae756805c54d1217d2714ffba28aa14a535b97a
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-06 22:23:21 +0300

    Update Main.java

--- TASK5/src/Main.java
+++ TASK5/src/Main.java
@@ -14,8 +14,6 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         SizeException.handleRuntimeException();
     }
-
 }
