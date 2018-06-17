0c6abafce0171c6557c6225e4e00e341786a6b52
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-17 23:12:52 +0300

    Add files via upload

---
+++ TASK4/src/HeadsAndTails.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Heads_n_Tails {
+    private final static int NUMBER_OF_SIDES = 2;
+
+    public static String calcHeadsTails(int numberOfThrows) throws Exception {
+
+        InvalidException.isInvalidException(number_of_throws);
+
+        int heads = 0;
+        int side = 0;
+
+        Random random = new Random();
+
+        for (int i = 1; i <= numberOfThrows; i++) {
+            side = random.nextInt(NUMBER_OF_SIDES);
+
+            if (side == 0) {
+                heads++;
+            } 
+        }
+        return "You threw the coin for: " + numberOfThrows + " times." + " Heads: " + heads + " Tails: "
+                  + numberOfThrows - heads;
+    }
+
+}
