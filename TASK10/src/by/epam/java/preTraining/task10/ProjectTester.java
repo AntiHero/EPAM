913352ac284904ee7101699bc866f61fe21e182a
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-22 00:10:54 +0300

    Add files via upload

---
+++ TASK10/src/by/epam/java/preTraining/task10/ProjectTester.java
@@ -0,0 +1,40 @@
+package by.epam.java.preTraining.Task10;
+
+import by.epam.java.preTraining.Task10.util.*;
+
+public class ProjectTester {
+    public static void calcPowerTest() {
+        double number = Randomizer.makeRand(5.);
+        int power = (int)Randomizer.makeRand(2.);
+        
+        View.print("-----Power-----");
+        View.print("Number: " + number + ", Power: " + power + ", Result: " + Recursion.calcPower(number, power));
+        
+    }
+    
+    public static void calcSumOfDigitsTest() {
+        int number = (int)Randomizer.makeRand(1000);
+        
+        View.print("\n-----Sum Of Digits-----");
+        View.print("Number: " + number + ", Sum: " + Recursion.calcSumOfDigits(number));
+    }
+    
+    public static void equalsTest() {
+        //int number = (int)Randomizer.makeRand(100);
+        //int sum = (int)Randomizer.makeRand(100);
+        
+        int number = 358;
+        int sum = 16;
+        
+        View.print("\n-----Equals-----");
+        View.print("First number: " + number + ", Pseudo sum: " + sum + ", Equals?:" + Recursion.equals(number, sum));
+    }
+    
+    public static void fibonacciTest() {
+        int index = (int)Randomizer.makeRand(20);
+        
+        View.print("\n------Fibonacci number------");
+        View.print("Index: " + index + " Fibonacci number: " + Recursion.getFibonacciNum(index));
+    }
+    
+}
---
+++ TASK10/src/by/epam/java/preTraining/task10/Recursion.java
@@ -0,0 +1,43 @@
+package by.epam.java.preTraining.Task10;
+
+public class Recursion {
+    static final int DECIMAL_BASE = 10;
+    static final int FIRST_FIBONACCI_NUMBER = 1;
+    static final int SECOND_FIBONACCI_NUMBER = 1;
+
+    public static int calcSumOfDigits(int number) {
+
+        if (number < 0)
+            return -1;
+
+        return number == 0 ? 0 : number % 10 + calcSumOfDigits(number / DECIMAL_BASE);
+    }
+
+    protected static double calcPower(double number, int power) {
+
+        if (number == 0 || power < 0)
+            return -1;
+
+        return power == 0 ? 1 : number * calcPower(number, power - 1);
+    }
+
+    public static boolean equals(int n, int s) {
+
+        if (n < 0 || s < 0)
+            return false;
+
+        return n == 0 ? s == 0 : equals(n / DECIMAL_BASE, s - n % DECIMAL_BASE);
+    }
+
+    public static int getFibonacciNum(int index) {
+            
+        if (index < 1) {
+            return -1;
+        } else if (index == 1) {
+            return FIRST_FIBONACCI_NUMBER;
+        } else if (index == 2) {
+            return SECOND_FIBONACCI_NUMBER;
+        }
+        return getFibonacciNum(index - 1) + getFibonacciNum(index - 2);
+    }
+}
