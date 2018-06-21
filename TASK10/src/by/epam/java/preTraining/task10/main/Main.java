2d0d1b91c080cc8bfb512e729a03a266cbc5c683
Aleksandr Schemelev <prophreaker@gmail.com>
2018-06-22 00:09:49 +0300

    Add files via upload

---
+++ TASK10/src/by/epam/java/preTraining/task10/main/Main.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package by.epam.java.preTraining.Task10.main;
+
+import by.epam.java.preTraining.Task10.util.View;
+import by.epam.java.preTraining.Task10.ProjectTester;
+import by.epam.java.preTraining.Task10.util.*;
+
+/**
+ * @author Schemelev
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        View.print("----Recursion Tester Program----");
+        
+        ProjectTester.calcPowerTest();
+        ProjectTester.calcSumOfDigitsTest();
+        ProjectTester.equalsTest();
+        ProjectTester.fibonacciTest();
+    }
+
+}
